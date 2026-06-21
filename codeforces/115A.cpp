87fc3041a1cd13e0096f0618fb77794d3ceb27a6
Aarav Shah <74358472+Aaravshah2907@users.noreply.github.com>
2026-06-21 19:29:00 +0530

    Add solution for problem 115A, including implementation and sample input/output

--- codeforces/115A.cpp
+++ codeforces/115A.cpp
@@ -23,13 +23,31 @@ bool ckmin(T &a, const T &b){
 }
 
 template <class T>
-bool ckmax(T &a, const T &b)
-{
+bool ckmax(T &a, const T &b){
     return a < b ? a = b, true : false;
 }
 
-void solve(){
 
+void solve(){
+    int n; cin >> n;
+    vector<int> boss(n + 1);
+    for(int i = 1; i <= n; i++){
+        cin >> boss[i];
+    }
+
+    vector<int> depth(n + 1, 0);
+    
+    function<int(int)> dfs = [&](int u){
+        if(depth[u] != 0) return depth[u];
+        if(boss[u] == -1) return depth[u] = 1;
+        return depth[u] = 1 + dfs(boss[u]);
+    };
+
+    int ans = 0;
+    for(int i = 1; i <= n; i++){
+        ans = max(ans, dfs(i));
+    }
+    cout << ans << nl;
 }
 
 int main(){
@@ -37,10 +55,8 @@ int main(){
     cin.tie(nullptr);
 
     int T = 1;
-    cin >> T;
 
-    while (T--)
-        solve();
-
+    //if (!(cin >> T)) return 0;
+    while (T--) solve();
     return 0;
 }
